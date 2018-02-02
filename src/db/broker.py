0af0b22bc92d57b796b622b5494714927eab0c8a
Alexander Lisianoi <all3fox@gmail.com>
2018-02-02 21:35:15 +0100

    Add DataLoader -> SlotLoader | DateLoader
    Removes previous SlotWorker API

--- src/db/broker.py
+++ src/db/broker.py
@@ -1,6 +1,6 @@
 import logging
 
-from datetime import datetime
+from datetime import datetime, timedelta, date
 from pathlib import Path
 
 from PyQt5.QtCore import *
@@ -11,99 +11,133 @@ from sqlalchemy.orm import sessionmaker, Session
 from src.db.model import Base, Tag, Task, Slot
 
 
-class SlotWorker(QObject):
+class DataLoader(QObject):
     '''
-    Make database query in background, use Qt signals to return results
+    Abstract class for database queries that happen in the background
+
+    Args:
+        session: SQLAlchemy database session instance
+        parent : parent object if Qt ownership is required
+    '''
+
+    # Emitted once data is written to the database
+    stored = pyqtSignal()
+    # Emitted once a list of data returns from the database query 
+    loaded = pyqtSignal(list)
+
+    # Emitted before the database query
+    started = pyqtSignal()
+    # Emitted after the database query
+    stopped = pyqtSignal()
+    # Emitted if there is an error at any point
+    errored = pyqtSignal()
+
+    def __init__(self, session: Session, parent: QObject=None):
+
+        super().__init__(parent)
+
+        self.session = session
+
+
+class SlotLoader(DataLoader):
+    '''
+    Perform a database query, return a limited amount of results
 
     Args:
         session  : SQLAlchemy database session instance
-        date_fst : datetime after which the slots should start
-        date_lst : datetime before which the slots should stop
-        slice_fst: first element to be returned [1]
-        slice_lst: first element to *not* be returned [1]
+        slice_fst: index of the first element to return [1]
+        slice_lst: index of the first element to *not* return [1]
         parent   : parent object if Qt ownership is required
-
-    Raises:
-        RuntimeError: if date_fst > date_list
 
     Note:
         [1]: See http://docs.sqlalchemy.org/en/latest/orm/query.html
              to learn more about SQLAlchemy batch processing
     '''
 
-    stored = pyqtSignal()
-    loaded = pyqtSignal(list)
-
-    started = pyqtSignal()
-    stopped = pyqtSignal()
-    errored = pyqtSignal()
-
     def __init__(
         self
         , session  : Session
-        , date_fst : datetime=None
-        , date_lst : datetime=None
         , slice_fst: int=0
         , slice_lst: int=100
         , parent   : QObject=None
     ):
 
-        super().__init__(parent)
+        super().__init__(session, parent)
 
-        if date_fst is not None and date_lst is not None:
-            if date_fst > date_lst:
-                raise RuntimeError('Must be date_fst <= date_lst')
-
-        self.session = session
-        self.date_fst, self.date_lst = date_fst, date_lst
-        self.slice_fst, self.slice_lst = slice_fst, slice_lst
-
-        self.logger = logging.getLogger('tslot')
-        self.logger.debug('SlotWorker has a logger')
+        self.slice_fst = slice_fst
+        self.slice_lst = slice_lst
 
     @pyqtSlot()
     def work(self):
-        self.logger.debug('About to emit .started')
         self.started.emit()
 
-        self.logger.debug('About to emit .loaded')
-        if self.date_fst is not None and self.date_lst is not None:
-            self.loaded.emit([
-                (tag, task, slot)
-                for tag, task, slot in self.session.query(
-                    Tag, Task, Slot
-                ).filter(
-                    Tag.tasks
-                    , Task.slots
-                    , self.date_fst <= Slot.fst
-                    , Slot.lst < self.date_lst
-                ).order_by(
-                    Slot.fst
-                ).slice(
-                    self.slice_fst, self.slice_lst
-                )
-            ])
-        else:
-            self.loaded.emit([
-                (tag, task, slot)
-                for tag, task, slot in self.session.query(
-                    Tag, Task, Slot
-                ).filter(
-                    Tag.tasks, Task.slots
-                ).order_by(
-                    Slot.fst
-                ).slice(
-                    self.slice_fst, self.slice_lst
-                )
-            ])
+        self.loaded.emit([
+            (tag, task, slot)
+            for tag, task, slot in self.session.query(
+                Tag, Task, Slot
+            ).filter(
+                Tag.tasks, Task.slots
+            ).order_by(
+                Slot.fst
+            ).slice(
+                self.slice_fst, self.slice_lst
+            )
+        ])
 
-        self.logger.debug('About to emit .stopped')
         self.stopped.emit()
 
 
-class SlotRunnable(QRunnable):
+class DateLoader(DataLoader):
+    '''
+    Perform a database query, return a limited amount of results
 
-    def __init__(self, worker: SlotWorker):
+    Args:
+        session : SQLAlchemy database session instance
+        date_fst: the first date to return
+        date_lst: the first date to *not* return
+        parent  : parent object if Qt ownership is required
+    '''
+
+    def __init__(
+        self
+        , session : Session
+        , date_fst: datetime=None
+        , date_lst: datetime=None
+        , parent  : QObject=None
+    ):
+
+        super().__init__(session, parent)
+
+        self.date_fst = date_fst
+        self.date_lst = date_lst
+
+    @pyqtSlot()
+    def work(self):
+        self.started.emit()
+
+        self.loaded.emit([
+            (tag, task, slot)
+            for tag, task, slot in self.session.query(
+                Tag, Task, Slot
+            ).filter(
+                Tag.tasks
+                , Task.slots
+                , self.date_fst <= Slot.fst
+                , Slot.lst < self.date_lst
+            ).order_by(
+                Slot.fst
+            )
+        ])
+
+        self.stopped.emit()
+
+
+class DataRunnable(QRunnable):
+    '''
+    Wrap a subclass of DataLoader and enable its execution in a thread
+    '''
+
+    def __init__(self, worker: DataLoader):
 
         super().__init__()
 
@@ -116,65 +150,121 @@ class SlotRunnable(QRunnable):
 
 class DataBroker(QObject):
     '''
-    Read and write to the underlying database
+    Provide (unique) database session and (unique) threadpool
 
     The database communication is dispatched to separate threads. This
-    allows to use DataBroker inside GUI code without GUI freezing.
+    means that the GUI can call potentially long-running operations and
+    not run the risk of freezing to death.
 
-    Note:
-        See Stylist as somewhat similar class
+    This class is supposed to be a singleton.
+
+    Args:
+        path  : full path to the database; If None, use default
+        parent: if Qt ownership is required, provides parent object
     '''
 
     def __init__(self, path: Path=None, parent: QObject=None):
-        '''
-        Create a (unique) database session and a (unique) threadpool
-
-        This class is supposed to be a singleton: provide one database
-        connection and one threadpool for parallel database queries.
-
-        Args:
-            path  : full path to the database; If None, use default
-            parent: if Qt ownership is required, provides parent object
-        '''
 
         super().__init__(parent)
 
         self.logger = logging.getLogger('tslot')
+        self.logger.debug(self.__class__.__name__ + ' has a logger')
 
         if path is None:
             path = Path(Path.cwd(), Path('tslot.db'))
 
-        self.logger.debug('About to create_engine({})'.format(path))
         self.engine = create_engine('sqlite:///{}'.format(path))
 
         self.sessionmaker = sessionmaker()
         self.sessionmaker.configure(bind=self.engine)
 
-        self.logger.debug('About to create database session')
         self.session = self.sessionmaker()
 
-        self.logger.debug('About to create threadpool')
         self.threadpool = QThreadPool()
 
     def load_slots(
-            self
-            , fn_loaded
-            , fn_started=None
-            , fn_stopped=None
-            , fn_errored=None
-            , slice_fst: int=0
-            , slice_lst: int=100
+        self
+        , fn_loaded
+        , fn_started=None
+        , fn_stopped=None
+        , fn_errored=None
+        , slice_fst: int=0
+        , slice_lst: int=100
     ):
         '''
-        Dispatch a slot loading task to an instance of a SlotWorker
+        Dispatch a slot loading task to an instance of a SlotLoader
+
+        Load not more than (slice_lst - slice_fst) time entries, so it
+        can be used to load rather large chunks to fit in memory.
 
         Args:
-            fn_loaded : a callback for the loaded signal (success)
+            fn_loaded : a callback for the loaded signal
             fn_started: a callback for the started signal
             fn_stopped: a callback for the stopped signal
             fn_errored: a callback for the errored signal
-            slice_fst : the first slot to actually return
-            slice_lst : the first slot to *not* actually return
+            slice_fst : the first slot to return
+            slice_lst : the first slot to *not* return
+        '''
+
+        self.dispatch_worker(
+            SlotLoader(
+                session=self.session
+                , slice_fst=date_fst
+                , slice_lst=date_lst
+                , parent=self
+            )
+            , fn_loaded
+            , fn_started
+            , fn_stopped
+            , fn_errored
+        )
+
+    def load_dates(
+        self
+        , fn_loaded
+        , fn_started=None
+        , fn_stopped=None
+        , fn_errored=None
+        , date_fst: date=datetime.utcnow().date()
+        , date_lst: date=datetime.utcnow().date() + timedelta(days=1)
+    ):
+        '''
+        Dispatch a slot loading task to an instance of a DateLoader
+
+        Load *all* the time entries between the given dates
+
+        Args:
+            fn_loaded : a callback for the loaded signal
+            fn_started: a callback for the started signal
+            fn_stopped: a callback for the stopped signal
+            fn_errored: a callback for the errored signal
+            slice_fst : the first slot to return
+            slice_lst : the first slot to *not* return
+        '''
+
+        self.dispatch_worker(
+            DateLoader(
+                session=self.session
+                , date_fst=date_fst
+                , date_lst=date_lst
+                , parent=self
+            )
+            , fn_loaded
+            , fn_started
+            , fn_stopped
+            , fn_errored
+        )
+
+    def dispatch_worker(
+        self
+        , worker: DataLoader
+        , fn_loaded
+        , fn_started=None
+        , fn_stopped=None
+        , fn_errored=None
+    ):
+        '''
+        Connect worker signals to slot callbacks and start its thread
         '''
 
         if fn_started is None:
@@ -184,20 +274,13 @@ class DataBroker(QObject):
         if fn_errored is None:
             fn_errored = self.fn_errored
 
-        worker = SlotWorker(
-            session=self.session
-            , slice_fst=slice_fst
-            , slice_lst=slice_lst
-            , parent=self
-        )
-
         worker.started.connect(fn_started)
         worker.stopped.connect(fn_stopped)
         worker.errored.connect(fn_errored)
 
         worker.loaded.connect(fn_loaded)
 
-        self.threadpool.start(SlotRunnable(worker))
+        self.threadpool.start(DataRunnable(worker))
 
     def store_slots(self):
         raise NotImplementedError()

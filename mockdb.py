77e5ee49e2cf001d6354938b9b45c30c142fd24e
Alexander Lisianoi <all3fox@gmail.com>
2018-04-07 23:40:31 +0200

    Update mockdb.py

--- mockdb.py
+++ mockdb.py
@@ -6,8 +6,7 @@ from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from src.db.model import Base, TagModel
-from src.db.model import DateModel, TaskModel, SlotModel
+from src.db.model import Base, SlotModel, TaskModel, TagModel
 
 
 def create_a_day(session, delta):
@@ -40,70 +39,60 @@ def create_a_day(session, delta):
 
     base = datetime.utcnow() - delta
 
-    date, time = base.date(), base.time()
-
-    date = DateModel(date=date)
-
     aerobic_slot0 = SlotModel(
         task=aerobic_task
-        , fst=time.replace(hour=8, minute=0, second=0)
-        , lst=time.replace(hour=9, minute=0, second=0)
+        , fst=base.replace(hour=8, minute=0, second=0)
+        , lst=base.replace(hour=9, minute=0, second=0)
     )
 
     anaerobic_slot0 = SlotModel(
         task=anaerobic_task
-        , fst=time.replace(hour=21, minute=30, second=0)
-        , lst=time.replace(hour=22, minute=0 , second=0)
+        , fst=base.replace(hour=21, minute=30, second=0)
+        , lst=base.replace(hour=22, minute=0 , second=0)
     )
 
     shop_slot0 = SlotModel(
         task=shop_task
-        , fst=time.replace(hour=11, minute=4, second=2)
-        , lst=time.replace(hour=13, minute=8, second=9)
+        , fst=base.replace(hour=11, minute=4, second=2)
+        , lst=base.replace(hour=13, minute=8, second=9)
     )
 
     wash_slot0 = SlotModel(
         task=wash_task
-        , fst=time.replace(hour=13, minute=20, second=44)
-        , lst=time.replace(hour=13, minute=55, second=18)
+        , fst=base.replace(hour=13, minute=20, second=44)
+        , lst=base.replace(hour=13, minute=55, second=18)
     )
 
     movies_slot0 = SlotModel(
         task=movies_task
-        , fst=time.replace(hour=19, minute=0, second=0)
-        , lst=time.replace(hour=20, minute=10, second=0)
+        , fst=base.replace(hour=19, minute=0, second=0)
+        , lst=base.replace(hour=20, minute=10, second=0)
     )
 
     internet_slot0 = SlotModel(
         task=internet_task
-        , fst=time.replace(hour=15, minute=20, second=2)
-        , lst=time.replace(hour=18, minute=33, second=2)
+        , fst=base.replace(hour=15, minute=20, second=2)
+        , lst=base.replace(hour=18, minute=33, second=2)
     )
 
     cook_slot0 = SlotModel(
         task=cook_task
-        , fst=time.replace(hour=10, minute=10, second=10)
-        , lst=time.replace(hour=10, minute=43, second=32)
+        , fst=base.replace(hour=10, minute=10, second=10)
+        , lst=base.replace(hour=10, minute=43, second=32)
     )
 
     cook_slot1 = SlotModel(
         task=cook_task
-        , fst=time.replace(hour=14, minute=19, second=43)
-        , lst=time.replace(hour=14, minute=55, second=22)
+        , fst=base.replace(hour=14, minute=19, second=43)
+        , lst=base.replace(hour=14, minute=55, second=22)
     )
 
     cook_slot2 = SlotModel(
         task=cook_task
-        , fst=time.replace(hour=20, minute=30, second=0)
-        , lst=time.replace(hour=21, minute=10, second=1)
+        , fst=base.replace(hour=20, minute=30, second=0)
+        , lst=base.replace(hour=21, minute=10, second=1)
     )
 
-    date.slots = [
-        cook_slot0, cook_slot1, cook_slot2, aerobic_slot0, shop_slot0,
-        anaerobic_slot0, wash_slot0, movies_slot0, internet_slot0
-    ]
-
-    session.add(date)
     session.add_all([chores_tag, workout_tag, freetime_tag])
     session.add_all([
         cook_task, shop_task, wash_task, aerobic_task, anaerobic_task,

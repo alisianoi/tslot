3ee0f0018cf6ae33b142f19fb64402d3bd33396b
Alexander Lisianoi <all3fox@gmail.com>
2018-05-31 15:56:58 +0200

    Add a simple shortcut example

---
+++ poc/shortcut_poc.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import sys
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class MyMainWidget(QWidget):
+
+    def __init__(self, parent: QWidget=None) -> None:
+
+        super().__init__(parent)
+
+        self.trigger_shortcut = QShortcut(
+            QKeySequence(Qt.CTRL + Qt.Key_P), self
+        )
+
+        self.trigger_shortcut.activated.connect(
+            self.tell_me
+        )
+
+    @pyqtSlot()
+    def tell_me(self):
+        print('BONANZA!')
+
+
+class MyMainWindow(QMainWindow):
+
+    def __init__(self, parent: QWidget=None) -> None:
+
+        super().__init__(parent)
+
+        self.widget = MyMainWidget()
+
+        self.setCentralWidget(self.widget)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    main_window = MyMainWindow()
+    main_window.show()
+
+    sys.exit(app.exec())

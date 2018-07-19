fa03e0699fc9b1e3f50de4d1313194abd2500400
Alexander Lisianoi <all3fox@gmail.com>
2018-07-19 22:54:07 +0200

    Simplify base.py messages

--- src/msg/base.py
+++ src/msg/base.py
@@ -2,16 +2,14 @@ import logging
 
 
 class TMessage:
-    '''
-    Base class for all messages
-    '''
+    """Base class for all messages"""
+
+    def __init__(self):
+
+        self.logger = logging.getLogger('tslot')
 
     def key_val_or_key_len(self, key, val):
-        '''
-        Return a reasonable key-value string
-
-        If the value is too long, truncate it
-        '''
+        """Help show (reasonably truncated) key/value pairs to a human"""
 
         if isinstance(val, list) and len(val) > 3:
             return f'len({key})={len(val)}'
@@ -29,62 +27,18 @@ class TMessage:
         return self.__class__.__name__ + '(' + args + ')'
 
 class TFailure(TMessage):
-    '''
-    Base class for all failure messages
-    '''
+    """Base class for all failure messages"""
 
     def __init__(self, message):
 
-        self.name = self.__class__.__name__
-        self.logger = logging.getLogger('tslot')
-
         self.message = message
-
-        self.logger.debug(self)
 
 class TRequest(TMessage):
     """Base class for all requests"""
 
-    def __init__(self):
-
-        self.logger = logging.getLogger('tslot')
-
+    pass
 
 class TResponse(TMessage):
     """Base class for all responses"""
 
     pass
-
-
-class TSimpleResponse(TResponse):
-    """Base class for all *simple* responses"""
-
-    pass
-
-class TComplexResponse(TResponse):
-    """
-    Base class for all *complex* responses
-
-    These responses are complex because they also contain the request that is
-    being responded to.
-
-    :param request: the original request message
-    """
-
-    def __init__(self, request: TRequest):
-
-        self.logger = logging.getLogger('tslot')
-
-        # Copy all parameters from the request into the response
-        msg = 'Collision of attribute name {} between {} and {}'
-
-        for key, val in request.__dict__.items():
-            if key == 'logger':
-                continue
-
-            if key in self.__dict__:
-                raise RuntimeError(
-                    msg.format(key, self.name, request.name)
-                )
-
-            self.__dict__[key] = val

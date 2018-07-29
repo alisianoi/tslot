036b8890ba3aebab7bb02212d02d47301909ff06
Alexander Lisianoi <all3fox@gmail.com>
2018-07-29 19:24:21 +0200

    Rename test_loader_for_slots into test_reader_for_slots

--- test/db/test_reader_for_slots.py
+++ test/db/test_reader_for_slots.py
@@ -1,12 +1,19 @@
 import pytest
 
-from src.db.loader_for_slots import TRaySlotLoader
+from src.db.reader_for_slots import TRaySlotReader
+from src.msg.fetch_slot import TRaySlotFetchRequest
 
 from test.db.test_loader import setup_one_slot_one_date
 from test.db.test_loader import setup_one_slot_whole_date
 from test.db.test_loader import setup_two_slots_one_date
 from test.db.test_loader import setup_two_slots_two_dates
 from test.db.test_loader import setup_four_slots_two_dates
+
+
+DEFAULT_DATES_DIR = 'future_to_past'
+DEFAULT_TIMES_DIR = 'past_to_future'
+DEFAULT_SLICE_FST = 0
+DEFAULT_SLICE_LST = 128
 
 
 @pytest.mark.parametrize('direction, total', [
@@ -16,19 +23,26 @@ def test_ray_date_loader_0(session, qtbot, direction, total):
 
     slots = setup_one_slot_whole_date(session)
 
-    worker = TRaySlotLoader(
+    request = TRaySlotFetchRequest(
         dt_offset=slots[0][0].add(days=1).start_of('day')
         , direction=direction
+        # Use default values for other parameters:
+        , dates_dir=DEFAULT_DATES_DIR
+        , times_dir=DEFAULT_TIMES_DIR
+        , slice_fst=DEFAULT_SLICE_FST
+        , slice_lst=DEFAULT_SLICE_LST
     )
+
+    worker = TRaySlotReader(request=request)
 
     worker.session = session
 
     def handle_loaded(entries):
         assert len(entries) == total
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -39,19 +53,26 @@ def test_ray_date_loader_1(session, qtbot, direction, total):
 
     slots = setup_one_slot_whole_date(session)
 
-    worker = TRaySlotLoader(
+    request = TRaySlotFetchRequest(
         dt_offset=slots[0][0].start_of('day')
         , direction=direction
+        # Use default values for other parameters:
+        , dates_dir=DEFAULT_DATES_DIR
+        , times_dir=DEFAULT_TIMES_DIR
+        , slice_fst=DEFAULT_SLICE_FST
+        , slice_lst=DEFAULT_SLICE_LST
     )
+
+    worker = TRaySlotReader(request=request)
 
     worker.session = session
 
     def handle_loaded(entries):
         assert len(entries) == total
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -62,7 +83,7 @@ def test_ray_date_loader_2(session, qtbot, direction, total):
 
     slots = setup_one_slot_whole_date(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[0][0].subtract(days=1).start_of('day')
         , direction=direction
     )
@@ -72,9 +93,9 @@ def test_ray_date_loader_2(session, qtbot, direction, total):
     def handle_loaded(entries):
         assert len(entries) == total
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -85,7 +106,7 @@ def test_ray_date_loader_3(session, qtbot, direction, total):
 
     slots = setup_one_slot_one_date(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[0][0].add(days=1).start_of('day')
         , direction=direction
     )
@@ -95,9 +116,9 @@ def test_ray_date_loader_3(session, qtbot, direction, total):
     def handle_loaded(entries):
         assert len(entries) == total
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -108,7 +129,7 @@ def test_ray_date_loader_4(session, qtbot, direction, total):
 
     slots = setup_one_slot_one_date(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[0][0].start_of('day')
         , direction=direction
     )
@@ -118,9 +139,9 @@ def test_ray_date_loader_4(session, qtbot, direction, total):
     def handle_loaded(entries):
         assert len(entries) == total
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -131,7 +152,7 @@ def test_ray_date_loader_5(session, qtbot, direction, total):
 
     slots = setup_one_slot_one_date(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[0][0].subtract(days=1).start_of('day')
         , direction=direction
     )
@@ -141,9 +162,9 @@ def test_ray_date_loader_5(session, qtbot, direction, total):
     def handle_loaded(entries):
         assert len(entries) == total
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -156,7 +177,7 @@ def test_ray_date_loader_times_dir_0(
 
     slots = setup_two_slots_one_date(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[-1][-1].add(days=1).start_of('day')
         , direction='future_to_past'
         , times_dir=times_dir
@@ -174,9 +195,9 @@ def test_ray_date_loader_times_dir_0(
             assert entry[0].fst == slot[0]
             assert entry[0].lst == slot[1]
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -189,7 +210,7 @@ def test_ray_date_loader_times_dir_1(
 
     slots = setup_two_slots_one_date(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[-1][-1].subtract(days=1).start_of('day')
         , direction='past_to_future'
         , times_dir=times_dir
@@ -207,9 +228,9 @@ def test_ray_date_loader_times_dir_1(
             assert entry[0].fst == slot[0]
             assert entry[0].lst == slot[1]
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -222,7 +243,7 @@ def test_ray_date_loader_dates_dir_0(
 
     slots = setup_two_slots_two_dates(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[-1][-1].add(days=1).start_of('day')
         , direction='future_to_past'
         , dates_dir=dates_dir
@@ -240,9 +261,9 @@ def test_ray_date_loader_dates_dir_0(
             assert entry[0].fst == slot[0]
             assert entry[0].lst == slot[1]
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -255,7 +276,7 @@ def test_ray_date_loader_dates_dir_1(
 
     slots = setup_two_slots_two_dates(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[0][0].subtract(days=1).start_of('day')
         , direction='past_to_future'
         , dates_dir=dates_dir
@@ -273,9 +294,9 @@ def test_ray_date_loader_dates_dir_1(
             assert entry[0].fst == slot[0]
             assert entry[0].lst == slot[1]
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -283,7 +304,7 @@ def test_ray_date_loader_slice_0(session, qtbot):
 
     slots = setup_two_slots_two_dates(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[-1][-1].add(days=1).start_of('day')
         , direction='future_to_past'
         , slice_fst=0
@@ -295,9 +316,9 @@ def test_ray_date_loader_slice_0(session, qtbot):
     def handle_loaded(entries):
         assert len(entries) == 1
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -305,7 +326,7 @@ def test_ray_date_loader_slice_1(session, qtbot):
 
     slots = setup_two_slots_two_dates(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[0][0].subtract(days=1).start_of('day')
         , direction='past_to_future'
         , slice_fst=0
@@ -317,9 +338,9 @@ def test_ray_date_loader_slice_1(session, qtbot):
     def handle_loaded(entries):
         assert len(entries) == 1
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -327,7 +348,7 @@ def test_ray_date_loader_slice_2(session, qtbot):
 
     slots = setup_four_slots_two_dates(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[-1][-1].add(days=1).start_of('day')
         , direction='future_to_past'
         , slice_fst=0
@@ -339,9 +360,9 @@ def test_ray_date_loader_slice_2(session, qtbot):
     def handle_loaded(entries):
         assert len(entries) == 2
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()
 
 
@@ -349,7 +370,7 @@ def test_ray_date_loader_slice_3(session, qtbot):
 
     slots = setup_four_slots_two_dates(session)
 
-    worker = TRaySlotLoader(
+    worker = TRaySlotReader(
         dt_offset=slots[0][0].subtract(days=1).start_of('day')
         , direction='past_to_future'
         , slice_fst=0
@@ -361,7 +382,7 @@ def test_ray_date_loader_slice_3(session, qtbot):
     def handle_loaded(entries):
         assert len(entries) == 2
 
-    worker.loaded.connect(handle_loaded)
+    worker.fetched.connect(handle_loaded)
 
-    with qtbot.waitSignal(worker.loaded, timeout=1000) as blocker:
+    with qtbot.waitSignal(worker.fetched, timeout=1000) as blocker:
         worker.work()

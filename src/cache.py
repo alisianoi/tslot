fa4828633a2bc7d88f26a17a8ded90b7fb9eb985
Alexander Lisianoi <all3fox@gmail.com>
2018-08-07 07:12:04 +0200

    Simplify cache

--- src/cache.py
+++ src/cache.py
@@ -1,28 +1,6 @@
-from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
-
-from src.msg.base import TRequest, TResponse, TFailure
+from src.ai.base import TObject
 
 
-class TCacheBroker(QObject):
+class TCacheBroker(TObject):
 
-    requested = pyqtSignal(TRequest)
-    responded = pyqtSignal(TResponse)
-    triggered = pyqtSignal(TFailure)
-
-    def __init__(self, parent: QObject=None):
-
-        super().__init__(parent)
-
-    @pyqtSlot(TRequest)
-    def handle_requested(self, request: TRequest):
-        # Connected to TWidget.requested
-        self.requested.emit(request)
-
-    @pyqtSlot(TResponse)
-    def handle_responded(self, response: TResponse):
-        # Connected to TDiskBroker.responded
-        self.responded.emit(response)
-
-    @pyqtSlot(TFailure)
-    def handle_triggered(self, failure: TFailure):
-        self.failed.emit(failure)
+    pass # The cache layer is not implemented yet

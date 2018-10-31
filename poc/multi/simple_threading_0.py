355e347a2f032e237df65d5f9c8928e5e75a7868
Aleksandr Lisianoi <alisianoi@protonmail.com>
2018-10-31 16:21:38 +0100

    Add scripts to understand concurrency

---
+++ poc/multi/simple_threading_0.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import time
+
+from datetime import datetime
+from threading import Thread
+
+def foo(value: int, delay: int):
+
+    time.sleep(delay)
+
+    print(value)
+
+
+if __name__ == '__main__':
+
+    print('===== About to start the wall clock test ======')
+
+    threads = [
+        Thread(target=foo, args=(i, i)) for i in range(3)
+    ]
+
+    print(f'About to start: {datetime.now()}')
+
+    for thread in threads:
+        thread.start()
+        thread.join()
+
+    print(f'About to finish: {datetime.now()}')
+
+    print("===== About to start the print sequence test =====")
+
+    threads = [
+        Thread(target=foo, args=(i, 0)) for i in range(50)
+    ]
+
+    for thread in threads:
+        thread.start()
+        thread.join()

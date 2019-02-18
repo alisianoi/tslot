8344a7f8896d715a7a07a5f16c7e0613843f987b
Aleksandr Lisianoi <alisianoi@protonmail.com>
2019-02-18 17:28:34 +0100

    Add ProgressAnimation to common/animation

---
+++ src/client/common/animation/progress.py
@@ -0,0 +1,26 @@
+from PyQt5.QtCore import QObject, QVariantAnimation
+
+from logger import logger
+
+
+class ProgressAnimation(QVariantAnimation):
+    """Create an animated progress value and change it X times per second."""
+
+    def __init__(self, duration: int, loop_count: int = -1, fps: int = 60, **kwargs):
+        super().__init__(**kwargs)
+
+        # Expect to be 24, 30 or 60
+        self.fps = fps
+
+        self.progress = 0
+
+        # Expect to be milliseconds, i.e. 1 second is 1000 milliseconds
+        self.setDuration(duration)
+        self.setStartValue(0)
+        self.setEndValue(int(duration / 1000 * fps))
+        self.setLoopCount(loop_count)
+
+        self.valueChanged.connect(self.handle_value_changed)
+
+    def handle_value_changed(self, value: int) -> None:
+        self.progress = value / self.endValue()

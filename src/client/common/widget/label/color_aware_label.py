ce25bca3b214fc00da7a7ee6ed5dc28f672872f1
Aleksandr Lisianoi <alisianoi@protonmail.com>
2019-02-19 15:39:59 +0100

    Refactor color aware label/line edit/push button to widget

--- src/client/common/widget/label/color_aware_label.py
+++ src/client/common/widget/label/color_aware_label.py
@@ -1,89 +1,28 @@
 from PyQt5.QtWidgets import QLabel
 
-from src.client.srv_color.service.color import TColorService
+from src.client.common.widget import *
 
 
-class TColorAwareLabel(QLabel):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self._color_service = TColorService()
-
-    def set_style_sheet(self, class_name: str, fg_color: str, bg_color: str):
-        self.setStyleSheet(f"""
-            {class_name} {{
-                color: {fg_color};
-                background-color: {bg_color};
-            }}"""
-        )
-
-
-class TPrimaryColorAwareLabel(TColorAwareLabel):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        self._color_service.fg_color_fst_lgt_changed.connect(
-            self._handle_style_sheet_changed
-        )
-        self._color_service.bg_color_fst_lgt_changed.connect(
-            self._handle_style_sheet_changed
-        )
-
-        self._handle_style_sheet_changed()
-
-    def _handle_style_sheet_changed(self):
-        self.set_style_sheet(
-            self.__class__.__name__
-            , self._color_service.fg_color_fst_lgt
-            , self._color_service.bg_color_fst_lgt
-        )
-
-
-class TAlternateColorAwareLabel(TColorAwareLabel):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        self._color_service.fg_color_snd_lgt_changed.connect(
-            self._handle_style_sheet_changed
-        )
-        self._color_service.bg_color_snd_lgt_changed.connect(
-            self._handle_style_sheet_changed
-        )
-
-        self._handle_style_sheet_changed()
-
-    def _handle_style_sheet_changed(self):
-        self.set_style_sheet(
-            self.__class__.__name__
-            , self._color_service.fg_color_snd_lgt
-            , self._color_service.bg_color_snd_lgt
-        )
-
-
-class TSuccessColorAwareLabel(TAlternateColorAwareLabel):
+class TColorAwareLabel(QLabel, TColorAwareWidget):
 
     pass
 
 
-class TFailureColorAwareLabel(TColorAwareLabel):
+class TPrimaryColorAwareLabel(QLabel, TPrimaryColorAwareWidget):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    pass
 
-        self._color_service.fg_color_err_changed.connect(
-            self._handle_style_sheet_changed
-        )
-        self._color_service.bg_color_err_changed.connect(
-            self._handle_style_sheet_changed
-        )
 
-        self._handle_style_sheet_changed()
+class TAlternateColorAwareLabel(QLabel, TAlternateColorAwareWidget):
 
-    def _handle_style_sheet_changed(self):
-        self.set_style_sheet(
-            self.__class__.__name__
-            , self._color_service.fg_color_err
-            , self._color_service.bg_color_err
-        )
+    pass
+
+
+class TSuccessColorAwareLabel(QLabel, TSuccessColorAwareWidget):
+
+    pass
+
+
+class TFailureColorAwareLabel(QLabel, TFailureColorAwareWidget):
+
+    pass
---
+++ src/client/common/widget/line_edit/color_aware_line_edit.py
@@ -0,0 +1,28 @@
+from PyQt5.QtWidgets import QLineEdit
+
+from src.client.common.widget import *
+
+
+class TColorAwareLineEdit(QLineEdit, TColorAwareWidget):
+
+    pass
+
+
+class TPrimaryColorAwareLineEdit(QLineEdit, TPrimaryColorAwareWidget):
+
+    pass
+
+
+class TAlternateColorAwareLineEdit(QLineEdit, TAlternateColorAwareWidget):
+
+    pass
+
+
+class TSuccessColorAwareLineEdit(QLineEdit, TSuccessColorAwareWidget):
+
+    pass
+
+
+class TFailureColorAwareLineEdit(QLineEdit, TFailureColorAwareWidget):
+
+    pass

e990c20bee92413417b1132aa68130d2163f2da7
Aleksandr Lisianoi <alisianoi@protonmail.com>
2019-02-20 14:15:27 +0100

    Make timer label/line edit react to font change

--- src/client/wgt_timer/widget/label/__init__.py
+++ src/client/wgt_timer/widget/label/__init__.py
@@ -1,17 +1,29 @@
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont, QFontDatabase
 from PyQt5.QtWidgets import QLabel
 
-from src.client.common.widget.label.animated_label import TAnimatedLabel
-from src.client.common.widget.label.color_aware_label import TPrimaryColorAwareLabel
-from src.client.common.widget.label.font_aware_label import TFontAwareLabel
+from src.client.common.widget.label.animated_label import *
+from src.client.common.widget.label.color_aware_label import *
+from src.client.common.widget.label.font_aware_label import *
 
 
-class TTimerLabel(TAnimatedLabel, TFontAwareLabel, TPrimaryColorAwareLabel):
+class TTimerLabel(TFontAwareLabel, TPrimaryColorAwareLabel):
 
     TIMER_IS_ZERO = '00:00:00'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.setAlignment(Qt.AlignCenter)
+        self.setContentsMargins(20, 0, 20, 0)
         self.setText(self.TIMER_IS_ZERO)
+        self.setAlignment(Qt.AlignCenter)
+
+        self.setup_font()
+        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+
+    def setup_font(self):
+        self.setFont(
+            QFontDatabase().font(
+                'Inconsolata', 'Bold', 4 * self._font_service.font_monospace_size / 3
+            )
+        )
--- src/client/wgt_timer/widget/line_edit/__init__.py
+++ src/client/wgt_timer/widget/line_edit/__init__.py
@@ -1,14 +1,17 @@
+import time
+
 from PyQt5.QtCore import QSize
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QFontDatabase
 
 from src.client.common.widget.common.font_aware_widget import TFontAwareWidget
 from src.client.common.widget.line_edit.color_aware_line_edit import \
     TAlternateColorAwareLineEdit
-from src.common.logger import logged
+from src.common.logger import logged, logmain
 
 
 class TTimerLineEdit(TAlternateColorAwareLineEdit, TFontAwareWidget):
 
+    @logged(logger=logmain, disabled=True)
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -19,14 +22,18 @@ class TTimerLineEdit(TAlternateColorAwareLineEdit, TFontAwareWidget):
             self._handle_base_height_changed
         )
 
+    @logged(logger=logmain, disabled=True)
     def _handle_font_loaded(self):
         self.setup_font()
 
     def _handle_base_height_changed(self):
         self.updateGeometry()
 
+    @logged(logger=logmain, disabled=True)
     def setup_font(self):
-        self.setFont(QFont('Quicksand', pointSize=self._font_service.font_serif_size))
+        self.setFont(
+            QFontDatabase().font('Quicksand', 'Bold', self._font_service.font_serif_size)
+        )
 
     @logged(disabled=True)
     def sizeHint(self) -> QSize:
